07d2c837bd855021bdce7f50eb565f2300df0857
Cullen Middleton <cullen.middleton@yoco.com>
2022-02-25 12:50:25 +0200

    Add basic mock database functionality
    With this commit, the basic behaviour of adding and removing is added
    to the mock database. Error checking is included in this basic
    functionality. Behaviour is currently serial number based, for
    simplicity sake.

--- app/src/main/java/DeviceOnboarding/MockDeviceDB.java
+++ app/src/main/java/DeviceOnboarding/MockDeviceDB.java
@@ -1,5 +1,65 @@
 package DeviceOnboarding;
 
 public class MockDeviceDB {
-    
+    private int deviceCount = 0;
+    private String serialNumbers[] = new String[10];
+
+    public void addDevice(String serialNo) {
+        if (isIllegalSerialNumber(serialNo)) {
+            throw new IllegalSerialNumberException();
+        }
+
+        serialNumbers[deviceCount++] = serialNo;
+    }
+
+    public boolean contains(String serialNo) {
+        if (isIllegalSerialNumber(serialNo)) {
+            throw new IllegalSerialNumberException();
+        }
+
+        return getIndex(serialNo) != -1;
+    }
+
+    public void removeDevice(String serialNo) {
+        if (isIllegalSerialNumber(serialNo)) {
+            throw new IllegalSerialNumberException();
+        }
+
+        if (isEmpty() || !contains(serialNo)) {
+            throw new IllegalRemoveException();
+        }
+
+        updateDeviceStorage(serialNo);
+    }
+
+    public boolean isEmpty() {
+        return deviceCount == 0;
+    }
+
+    private boolean isIllegalSerialNumber(String serialNo) {
+        return !serialNo.matches("\\d{4}-\\d{3}[\\dxX]");
+    }
+
+    private int getIndex(String serialNo) {
+        for (int i = 0; i < deviceCount; i++) {
+            if (serialNumbers[i].equals(serialNo)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private void updateDeviceStorage(String serialNo) {
+        for (int i = getIndex(serialNo) + 1; i < deviceCount; i++) {
+            serialNumbers[i - 1] = serialNumbers[i];
+            serialNumbers[i] = null;
+        }
+
+        deviceCount--;
+    }
+
+    public class IllegalRemoveException extends RuntimeException {};
+
+    public class IllegalSerialNumberException extends RuntimeException {};
 }
--- app/src/test/java/DeviceOnboarding/MockDeviceDBTest.java
+++ app/src/test/java/DeviceOnboarding/MockDeviceDBTest.java
@@ -1,5 +1,88 @@
 package DeviceOnboarding;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import DeviceOnboarding.MockDeviceDB.IllegalRemoveException;
+import DeviceOnboarding.MockDeviceDB.IllegalSerialNumberException;
+
 public class MockDeviceDBTest {
+    private MockDeviceDB mdb;
+
+    @BeforeEach
+    public void beforeEach() {
+        mdb = new MockDeviceDB();
+    }
     
+    @Test
+    public void initTest() {
+        assertTrue(mdb.isEmpty());
+    }
+
+    @Test
+    public void addDeviceTest() {
+        mdb.addDevice("2049-3630");
+        assertFalse(mdb.isEmpty());
+    }
+
+    @Test
+    public void removeDeviceTest() {
+        mdb.addDevice("2049-3630");
+        mdb.removeDevice("2049-3630");
+        assertTrue(mdb.isEmpty());
+    }
+
+    @Test
+    public void illegalSerialNumberTest() {
+        assertThrows(IllegalSerialNumberException.class, () -> {
+            mdb.addDevice("");
+        });
+
+        assertThrows(IllegalSerialNumberException.class, () -> {
+            mdb.removeDevice("");
+        });
+    }
+
+    @Test
+    public void illegalRemoveOnEmptyDBTest() {
+        assertThrows(IllegalRemoveException.class, () -> {
+            mdb.removeDevice("2049-3630");
+        });
+    }
+
+    @Test
+    public void illegalRemoveNonExistentDeviceTest() {
+        mdb.addDevice("2049-3630");
+
+        assertThrows(IllegalRemoveException.class, () -> {
+            mdb.removeDevice("2049-3631");
+        });
+    }
+
+    @Test
+    public void addRemoveAndTryToRemoveAgainTest() {
+        mdb.addDevice("2049-3630");
+        mdb.addDevice("2049-3631");
+        mdb.removeDevice("2049-3630");
+
+        assertThrows(IllegalRemoveException.class, () -> {
+            mdb.removeDevice("2049-3630");
+        });
+
+        mdb.removeDevice("2049-3631");
+    }
+
+    @Test
+    public void containsTest() {
+        mdb.addDevice("2049-3630");
+
+        assertTrue(mdb.contains("2049-3630"));
+        assertFalse(mdb.contains("2049-3631"));
+
+        assertThrows(IllegalSerialNumberException.class, () -> {
+            mdb.contains("2049-");
+        });
+    }
 }
