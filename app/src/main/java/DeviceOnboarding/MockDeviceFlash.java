02e56548f53d7446a57d9e749e0b4576f19f671e
Cullen Middleton <cullen.middleton@yoco.com>
2022-02-28 12:04:58 +0200

    Implement the mock device flashing

--- app/src/main/java/DeviceOnboarding/MockDeviceFlash.java
+++ app/src/main/java/DeviceOnboarding/MockDeviceFlash.java
@@ -1,9 +1,20 @@
 package DeviceOnboarding;
 
 public class MockDeviceFlash implements IFlashDevice{
+    int mode;
+
+    public MockDeviceFlash(int mode) {
+        this.mode = mode;
+    }
 
     @Override
     public boolean flashDevice() {
-        return false;
+        if (mode < 0) {
+            throw new FlashFailureException();
+        }
+
+        return mode == 0;
     }
+
+    public class FlashFailureException extends RuntimeException{}
 }
--- app/src/test/java/DeviceOnboarding/MockDeviceFlashTest.java
+++ app/src/test/java/DeviceOnboarding/MockDeviceFlashTest.java
@@ -1,5 +1,35 @@
 package DeviceOnboarding;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import DeviceOnboarding.MockDeviceFlash.FlashFailureException;
+
 public class MockDeviceFlashTest {
-    
+    @Test
+    public void workingFlashTest() {
+        MockDeviceFlash mdf = new MockDeviceFlash(0);
+
+        assertTrue(mdf.flashDevice());
+    }
+
+    @Test
+    public void failedButNotExceptionTest() {
+        MockDeviceFlash mdf = new MockDeviceFlash(1);
+        assertFalse(mdf.flashDevice());
+
+        mdf = new MockDeviceFlash(5);
+        assertFalse(mdf.flashDevice());
+    }
+
+    @Test
+    public void exceptionFailureTest() {
+        assertThrows(FlashFailureException.class, () -> {
+            (new MockDeviceFlash(-1)).flashDevice();
+        });
+
+        assertThrows(FlashFailureException.class, () -> {
+            (new MockDeviceFlash(-5)).flashDevice();
+        });
+    }
 }
